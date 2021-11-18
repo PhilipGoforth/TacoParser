c969f4110876f372f8bf52d9c600f6bdb04e20de
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-18 15:00:01 -0600

    final

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -34,13 +34,29 @@ namespace LoggingKata
             // Now that your Parse method is completed, START BELOW ----------
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.
+            ITrackable store1 = null;
+            ITrackable store2 = null;
             // Create a `double` variable to store the distance
+            double distance = 0.0;
 
             // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
 
             //HINT NESTED LOOPS SECTION---------------------
             // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
-
+            for (int i = 0; i < lines.Length; i++)
+            {
+                GeoCoordinate locA = new GeoCoordinate(locations[i].Location.Latitude, locations[i].Location.Longitude);
+                for (int j = 0; j < lines.Length; j++)
+                {
+                    GeoCoordinate locB = new GeoCoordinate(locations[j].Location.Latitude, locations[j].Location.Longitude);
+                    if (locA.GetDistanceTo(locB) > distance)
+                    {
+                        distance = locA.GetDistanceTo(locB);
+                        store1 = locations[i];
+                        store2 = locations[j];
+                    }
+                }
+            }
             // Create a new corA Coordinate with your locA's lat and long
 
             // Now, do another loop on the locations with the scope of your first loop, so you can grab the "destination" location (perhaps: `locB`)
@@ -51,7 +67,8 @@ namespace LoggingKata
             // If the distance is greater than the currently saved distance, update the distance and the two `ITrackable` variables you set above
 
             // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-
+            Console.WriteLine(store2.Name);
+            Console.WriteLine(store1.Name);
 
             
         }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -18,13 +18,17 @@
             if (cells.Length < 3)
             {
                 // Log that and return null
+                logger.LogInfo("Length not 3");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
             // grab the latitude from your array at index 0
+            var latitude = double.Parse(cells[0]);
             // grab the longitude from your array at index 1
+            var longitude = double.Parse(cells[1]);
             // grab the name from your array at index 2
+            var name = cells[2];
 
             // Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
@@ -33,12 +37,15 @@
             // that conforms to ITrackable
 
             // Then, you'll need an instance of the TacoBell class
+            var newTaco = new TacoBell() { Name = name};
+            Point location = new Point() { Latitude = latitude, Longitude = longitude};
+            newTaco.Location = location;
             // With the name and point set correctly
-
+            return newTaco;
             // Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
-            return null;
+            //return null;
         }
     }
 }
